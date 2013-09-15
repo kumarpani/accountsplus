5cdd1eb85c3f9c3f318df88785e09c1fea3e2b0b
Aravind M <arav.psgtech@gmail.com>
2013-09-15 21:59:52 +0530

    not allowing 0 balance in pending payments.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,11 +3,12 @@ class DashboardController < ApplicationController
 
   def index
     @upcoming_quotations = Quotation.where("event_date >= ? AND status == 'Confirmed'", Date.today).order("event_date ASC")
-    @confirmed_quotations = Quotation.where(status: 'Confirmed').group(:client_id).map { |quot|
+    @pending_payments = Quotation.where(status: 'Confirmed').group(:client_id).map { |quot|
       {client_name: quot.client.name,
        owes: quot.client.quotations
        .select { |q| q.status == 'Confirmed' }
        .sum { |q| q.total_price } -
            quot.client.payments.sum { |p| p.amount }} }
+    .select { |p| p[:owes] != 0.0 }
   end
 end
