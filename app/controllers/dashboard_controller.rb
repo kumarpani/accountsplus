2746a6f17c8b96ea7ef2b86253ad414a910ea367
Pani <kumarpani@gmail.com>
2013-10-21 23:27:05 +0530

    [Pani] Updated pending payments logic

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,10 +3,10 @@ class DashboardController < ApplicationController
 
   def index
     @upcoming_quotations = Quotation.where("event_date >= ? AND status == 'Confirmed'", Date.today).order("event_date ASC")
-    @pending_payments = Quotation.where("status == 'Invoice' OR status == 'Proforma Invoice'").group(:client_id).map { |quot|
+    @pending_payments = Quotation.where(status: 'Invoice').group(:client_id).map { |quot|
       {client: quot.client,
        owes: quot.client.quotations
-       .select { |q| q.status == 'Confirmed' }
+       .select { |q| q.status == 'Invoice' }
        .sum { |q| q.total_price } -
            quot.client.payments.sum { |p| p.amount }} }
     .select { |p| p[:owes] != 0.0 }
