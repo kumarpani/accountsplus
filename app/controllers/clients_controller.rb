2f8e9887a4d14c60ff103cc412b779b999b99833
Pani <kumarpani@gmail.com>
2013-10-25 19:03:48 +0530

    [Pani] Client Search

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -5,7 +5,7 @@ class ClientsController < ApplicationController
   # GET /clients
   # GET /clients.json
   def index
-    @clients = Client.all
+    @clients = Client.search(params[:search])
   end
 
   # GET /clients/1
--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,10 @@ class Client < ActiveRecord::Base
   has_many :payments, :dependent => :delete_all
 
   validates_presence_of :company_name, :contact_person_name
+
+  def self.search(search)
+    if search
+      find(:all, :conditions => ['company_name LIKE ? OR contact_person_name LIKE ? OR email LIKE ? OR phone_number LIKE ?', "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%"])
+    end
+  end
 end
