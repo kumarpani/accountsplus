73ef5779cfaf5b5361e6d770b7c9b47e524dcd2b
Pani <kumarpani@gmail.com>
2013-10-30 22:25:47 +0530

    [Pani] Sorting dashboard payment by owes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,5 +10,6 @@ class DashboardController < ApplicationController
        .sum { |q| q.total_price } -
            quot.client.payments.sum { |p| p.amount }} }
     .select { |p| p[:owes] != 0.0 }
+    @pending_payments = @pending_payments.sort_by {|o| o[:owes]}.reverse
   end
 end
