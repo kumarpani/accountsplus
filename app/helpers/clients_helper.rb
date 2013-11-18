20079bf3ed90d469fb568327439126ed759e56b6
Pani <kumarpani@gmail.com>
2013-11-18 20:12:04 +0530

    [Pani] Styling changes and helper functions

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,6 @@
 module ClientsHelper
+  def get_all_sorted_companies
+    Client.all.sort_by{|c| c.company_name.downcase}.map { |client| [client.company_name, client.id] }
+  end
+
 end
--- app/helpers/item_details_helper.rb
+++ app/helpers/item_details_helper.rb
@@ -1,2 +1,6 @@
 module ItemDetailsHelper
+  def get_all_sorted_particulars
+    ItemDetail.all.sort_by{|u| u.particulars.downcase}.uniq{|d| d[:particulars]}.map { |i| [i.id, i.particulars] }
+  end
+
 end
