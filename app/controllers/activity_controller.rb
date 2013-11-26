03d59ac60c3cbad8f58c2f79323cda369840b75c
Pani <kumarpani@gmail.com>
2013-11-26 23:49:26 +0530

    [Pani] Activity part 2

--- app/controllers/activity_controller.rb
+++ app/controllers/activity_controller.rb
@@ -10,7 +10,10 @@ class ActivityController < ApplicationController
 
     @invoices_raised = Quotation.where('invoice_raised_date >= ? AND invoice_raised_date <= ?', @s_date, @e_date).order(:invoice_raised_date).reverse_order
 
-    @payments = Payment.where()
+    @payments_changed = Payment.where('created_at >= ? AND created_at <= ?', @s_date, @e_date)
+    @payments_changed += Payment.where('updated_at >= ? AND updated_at <= ?', @s_date, @e_date)
+    @payments_changed = @payments_changed.sort_by {|p| p[:updated_at]}.reverse
+
   end
 
 end
