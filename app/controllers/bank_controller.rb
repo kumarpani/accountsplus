8d7a33f911eaa4fff6cf48ad429f91f44c33e271
Pani <kumarpani@gmail.com>
2014-01-05 14:36:23 +0530

    [Pani] Added show details in bank screen

--- app/controllers/bank_controller.rb
+++ app/controllers/bank_controller.rb
@@ -1,10 +1,7 @@
 class BankController < ApplicationController
-  def index
-    if !params[:start_date].to_s.empty? && !params[:end_date].to_s.empty?
-      @s_date = Date.parse(params[:start_date], '%d/%m/%Y')
-      @e_date = Date.parse(params[:end_date], '%d/%m/%Y')
 
-      @bank = Quotation.where('invoice_type = ? AND status = ? AND invoice_raised_date >= ? AND invoice_raised_date <= ?', INVOICE, INVOICE, @s_date, @e_date).order(:invoice_raised_date)
-    end
+  def index
+    @bank = Bank.new.index(params[:start_date], params[:end_date])
   end
+
 end
---
+++ app/models/bank.rb
@@ -0,0 +1,12 @@
+class Bank
+
+  def index(s_date, e_date)
+      if !s_date.to_s.empty? && !e_date.to_s.empty?
+        @s_date = Date.parse(s_date, '%d/%m/%Y')
+        @e_date = Date.parse(e_date, '%d/%m/%Y')
+
+        @bank = Quotation.where('invoice_type = ? AND status = ? AND invoice_raised_date >= ? AND invoice_raised_date <= ?', INVOICE, INVOICE, @s_date, @e_date).order(:invoice_number)
+      end
+    end
+end
+
