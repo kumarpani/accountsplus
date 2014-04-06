1cc68030cb26301c208ba42566ac0b0249b5b953
Pani <kumarpani@gmail.com>
2014-04-06 13:34:51 +0530

    [Pani] Templatize application values

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,25 @@
 module ApplicationHelper
 
+  # Application Title and Name
+  TITLE = 'AV Creations'
+  NAME = 'AV Creations'
+
+  # Address
+  ADD_LINE1 = '#6, 2rd Cross,'
+  ADD_LINE2 = 'C.S.I. Compound, Mission Road'
+  ADD_LINE3 = 'Bangalore - 5600 27'
+  TELEPHONE = '4205 8284 / 4205 8479'
+  MOBILE = '96864 54272/3/4'
+  EMAIL = 'avcreations12@gmail.com'
+
+  #Bank Details
+  BANK_ACC_NAME = 'AV Creations'
+  BANK_NAME_BRANCH = 'Indian Bank, Cubbonpet, Bangalore'
+  BANK_ACC_NUM = '860986003'
+  BANK_TYPE_OF_ACC = 'Current Account'
+  BANK_IFSC = 'IDIB 000C107'
+  BANK_MIRC = ''
+
   def display_verbose_date(date)
     date.to_date.strftime('%d %B, %Y')
   end
