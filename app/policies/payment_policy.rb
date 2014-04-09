63db03904b5a9a68b4f2b26347452b1fd995ac6b
Pani <kumarpani@gmail.com>
2014-04-09 22:23:26 +0530

    [Pani][F8] Amin Access

---
+++ app/policies/payment_policy.rb
@@ -0,0 +1,41 @@
+class PaymentPolicy
+  attr_reader :user, :payment
+
+  def initialize(user, payment)
+    @user = user
+    @payment = payment
+  end
+
+  #def index?
+  #  false
+  #end
+  #
+  #def show?
+  #  scope.where(:id => record.id).exists?
+  #end
+  #
+  #def create?
+  #  false
+  #end
+  #
+  #def new?
+  #  create?
+  #end
+
+  def update?
+    user.is_admin?
+  end
+
+  def edit?
+    false
+  end
+
+  def destroy?
+    user.is_admin?
+  end
+
+  #def scope
+  #  Pundit.policy_scope!(user, payment.class)
+  #end
+end
+
