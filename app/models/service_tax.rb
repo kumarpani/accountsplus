f9d906aad97a84ffdca6f494d2814efa82e836a0
Pani <kumarpani@gmail.com>
2014-04-18 19:03:53 +0530

    [F11]Tax exempted invoices

--- app/models/service_tax.rb
+++ app/models/service_tax.rb
@@ -5,7 +5,8 @@ class ServiceTax
         @s_date = Date.parse(s_date, '%d/%m/%Y')
         @e_date = Date.parse(e_date, '%d/%m/%Y')
 
-        Quotation.where('invoice_type = ? AND status = ? AND invoice_raised_date >= ? AND invoice_raised_date <= ?', INVOICE, INVOICE, @s_date, @e_date).order(:invoice_number)
+        Quotation.where('invoice_type IN (?, ?) AND status = ? AND invoice_raised_date >= ? AND invoice_raised_date <= ?',
+                        INVOICE_TAX, INVOICE_TAX_EXEMPTED, STATUS_INVOICE, @s_date, @e_date).order(:invoice_number)
       end
     end
 end
