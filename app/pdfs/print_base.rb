f9ba407bec72d8c59f6d46d3a73f398f4b01219a
Pani <kumarpani@gmail.com>
2014-04-29 23:56:16 +0530

    Format fixes

--- app/pdfs/print_base.rb
+++ app/pdfs/print_base.rb
@@ -1,13 +1,10 @@
 class PrintBase < Prawn::Document
 
 # Often-Used Constants
-  TABLE_ROW_COLORS = ["FFFFFF","DDDDDD"]
-  TABLE_FONT_SIZE = 9
-  TABLE_BORDER_STYLE = :grid
 
   def initialize()
     super(default_prawn_options={:left_margin => 105, :page_size => 'A4', :page_layout => :portrait, :right_margin => 2, :top_margin => 30})
-    font_size 10
+    font_size 9
   end
 
 
