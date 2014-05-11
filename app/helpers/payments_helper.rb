fcdf5418903006750b29ebcbf23ce5c09c53177c
Pani <kumarpani@gmail.com>
2014-05-11 19:43:53 +0530

    [Pani] Header changes

--- app/helpers/payments_helper.rb
+++ app/helpers/payments_helper.rb
@@ -1,10 +1,2 @@
 module PaymentsHelper
-  def get_header_for_payments(client_id, company_name)
-    if client_id.nil?
-      'All Payments'
-    else
-      'Payments for ' + company_name
-    end
-  end
-
 end
