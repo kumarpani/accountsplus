27aa41de0b675d5d8611c969c6f772245555fc68
Pani <kumarpani@gmail.com>
2014-05-17 12:02:39 +0530

    trying to fix change password mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,10 @@ AVCreations::Application.configure do
   config.action_mailer.default_url_options = { :host => 'audioplus.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default :charset => "utf-8"
+
 
   config.action_mailer.smtp_settings = {
       address: "smtp.gmail.com",
@@ -92,4 +95,5 @@ AVCreations::Application.configure do
       user_name: ENV["GMAIL_USERNAME"],
       password: ENV["GMAIL_PASSWORD"]
   }
+
 end
