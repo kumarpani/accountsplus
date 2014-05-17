adbed2014f3bcc1583d85ca9332c3a6d05bdcb9f
Pani <kumarpani@gmail.com>
2014-05-17 20:31:23 +0530

    [Pani] More mandatory fields

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,5 +2,5 @@ class Payment < ActiveRecord::Base
   belongs_to :client
   belongs_to :item_detail
 
-  validates_presence_of :paid_on, :amount
+  validates_presence_of :client_id, :mode, :paid_on, :amount, :payment_type
 end
