8a425c8e2a583bf3536acdb4a3683b15849fcf04
Pani <kumarpani@gmail.com>
2014-05-20 23:54:20 +0530

    [Pani] Removed remembarable in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable,
          #:registerable,
          :recoverable,
-         :rememberable, :trackable, :validatable,
+         #:rememberable, :trackable,
+         :validatable,
          :timeoutable
 
   def timeout_in
