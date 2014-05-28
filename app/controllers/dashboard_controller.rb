533218237e6f2871f863d50c01c6ce7834854c50
Pani <kumarpani@gmail.com>
2014-05-28 16:13:50 +0530

    [Pani] Dashboard to show balances from ledger

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,18 +1,18 @@
 class DashboardController < ApplicationController
 
   def index
+
     @tasks = Task.all.sort_by {|t| t[:created_at]}
+
     @upcoming_quotations = Quotation.where('event_date >= ? AND status = ?', Date.today, STATUS_CONFIRMED).order('event_date ASC')
-    @pending_payments = Quotation.select(:client_id).where(status: STATUS_INVOICE).group(:client_id).map { |quot|
-      {client: quot.client,
-       owes: quot.client.quotations
-       .select { |q| q.status == STATUS_INVOICE }
-       .sum { |q| q.total_price } -
-           quot.client.payments.select{|d| d.payment_type == 'Debit'}.sum { |p| p.amount } +
-           quot.client.payments.select{|c| c.payment_type == 'Credit'}.sum { |p| p.amount }
-      }
-    }
-    .select { |p| p[:owes] != 0.0 }
+
+    @pending_payments = Client.all.map {|c|
+       {
+           client: c,
+           owes: Ledger.new.get_ledger_balance(c.id)
+       }
+    }.select { |p| p[:owes] != 0.0 }
+
     @pending_payments = @pending_payments.sort_by {|o| o[:owes]}.reverse
   end
 end
