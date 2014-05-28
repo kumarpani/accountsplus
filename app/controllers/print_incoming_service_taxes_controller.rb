cf73bbd4b4ea3f277329bd92961c88ef863bd24b
Pani <kumarpani@gmail.com>
2014-05-28 20:19:48 +0530

    [Pani] Removed old way of printing
    Conflicts:
    	app/assets/stylesheets/print_quotations.css.scss
    	app/views/item_details/index.html.erb
    	app/views/print_quotations/index.html.erb
    	config/routes.rb

--- app/controllers/print_incoming_service_taxes_controller.rb
+++
@@ -1,8 +0,0 @@
-class PrintIncomingServiceTaxesController < ApplicationController
-  layout 'print'
-
-  def index
-    @incoming_service_taxes = IncomingServiceTax.new.index(params[:start_date], params[:end_date])
-  end
-
-end
--- app/controllers/print_service_taxes_controller.rb
+++
@@ -1,7 +0,0 @@
-class PrintServiceTaxesController < ApplicationController
-  layout 'print'
-
-  def index
-    @service_tax = ServiceTax.new.index(params[:start_date], params[:end_date])
-  end
-end
