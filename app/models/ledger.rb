c384e8169f3f4245ae88400d9c793dcbbcb76f0b
Pani <kumarpani@gmail.com>
2014-05-28 16:13:50 +0530

    [Pani] Dashboard to show balances from ledger

--- app/models/ledger.rb
+++ app/models/ledger.rb
@@ -2,26 +2,8 @@ class Ledger
 
   def index(s_date, e_date, client_id)
 
-    @client = Client.find(client_id)
+    @ledger_details = get_ledger(client_id)
 
-    @ledger_details = @client.quotations.keep_if { |q| q.status == STATUS_INVOICE }.map { |q|
-      {date: q.event_date, description: q.event_name, invoice_number: q.invoice_number, credit: q.total_price, id:q.id }
-    };
-
-    @ledger_details = @ledger_details + @client.payments.map { |p|
-      payment = {date: p.paid_on, description: (p.description || '') + ' ' + (p.mode || '')}
-      payment[p.payment_type.downcase.to_sym] = p.amount
-      payment
-    }
-
-    @ledger_details = @ledger_details + @client.incoming_service_taxes.map { |i|
-      ist = {date: i.invoice_date, description: ('InComing Service Tax: ') + (i.description || '')}
-      ist[:debit] = i.event_total
-      ist
-    }
-
-
-    @ledger_details.sort_by! { |l| l[:date] }
     balance = 0.0;
     @ledger_details = @ledger_details.map { |l|
       balance = l[:balance] = balance - (l[:debit] || 0.0) + (l[:credit] || 0.0)
@@ -38,7 +20,40 @@ class Ledger
     else
       @ledger_details = @ledger_details.select {|l| l[:date]}
     end
-
   end
+
+  def get_ledger(client_id)
+    @client = Client.find(client_id)
+
+    @ledger_details = @client.quotations.keep_if { |q| q.status == STATUS_INVOICE }.map { |q|
+      {date: q.event_date, description: q.event_name, invoice_number: q.invoice_number, credit: q.total_price, id: q.id}
+    };
+
+    @ledger_details = @ledger_details + @client.payments.map { |p|
+      payment = {date: p.paid_on, description: (p.description || '') + ' ' + (p.mode || '')}
+      payment[p.payment_type.downcase.to_sym] = p.amount
+      payment
+    }
+
+    @ledger_details = @ledger_details + @client.incoming_service_taxes.map { |i|
+      ist = {date: i.invoice_date, description: ('InComing Service Tax: ') + (i.description || '')}
+      ist[:debit] = i.event_total
+      ist
+    }
+
+    @ledger_details.sort_by! { |l| l[:date] }
+  end
+
+  def get_ledger_balance(client_id)
+    @ledger_details = get_ledger(client_id)
+
+    balance = 0.0;
+    @ledger_details = @ledger_details.map { |l|
+      balance = l[:balance] = balance - (l[:debit] || 0.0) + (l[:credit] || 0.0)
+      l
+    }
+    balance
+  end
+
 end
 
