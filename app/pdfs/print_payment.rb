85e05ab5c048a679308f531bda225b14b35ea509
kavya <kavyanagaraj.p@gmail.com>
2014-06-05 11:03:22 +0530

    [kavya] Populating values to Receipt and cash voucher and removing additonal column in activity page

--- app/pdfs/print_payment.rb
+++ app/pdfs/print_payment.rb
@@ -1,32 +1,35 @@
 class PrintPayment < PrintBase
 
- def initialize(payment_type)
+ def initialize(payment_id, email)
     super()
-    logo_and_address
-    header(payment_type)
 
-    if payment_type =='Debit'
-      receipt
+    payment = Payment.find(payment_id)
+    @user = User.current_user
+    logo_and_address
+    text(@user)
+    header(payment)
+
+    if payment.payment_type =='Debit'
+      receipt(payment, email)
     end
 
-    else if payment_type =='Credit'
-        cash_voucher
+    else if payment.payment_type =='Credit'
+        cash_voucher(payment, email)
          end
 
-  signature
+  # signature
  end
 
 
  def logo_and_address
-   column_box([0, cursor], :columns => 2, :width=>470, :height => 72) do
+   column_box([0, cursor], :columns => 2, :width=>470, :height => 60) do
 
-     image "#{Rails.root}/app/assets/images/logo.jpg", height: 72
+     image "#{Rails.root}/app/assets/images/logo.jpg", height: 60
 
      text("#{ApplicationHelper::ADD_LINE1}", align: :right)
      text("#{ApplicationHelper::ADD_LINE2}", align: :right)
      text("#{ApplicationHelper::ADD_LINE3}", align: :right)
      text("Tele Fax: #{ApplicationHelper::TELEPHONE}", align: :right)
-     text("Cell: #{ApplicationHelper::MOBILE}", align: :right)
      text("Email: #{ApplicationHelper::EMAIL}", align: :right)
 
    end
@@ -36,49 +39,47 @@ class PrintPayment < PrintBase
 
  end
 
- def header(payment_type)
+ def header(payment)
    text("\n")
    text("\n")
-   if payment_type =='Debit'
-     text("RECEIPT", :font_style => :bold, :size => 18, :align => :center)
+   if payment.payment_type =='Debit'
+     text("RECEIPT", :font_style => :bold, :size => 15, :align => :center)
    end
 
- else if payment_type =='Credit'
-        text("CASH VOUCHER", :font_style => :bold, :size => 18, :align => :center)
+ else if payment.payment_type =='Credit'
+        text("CASH VOUCHER", :font_style => :bold, :size => 15, :align => :center)
       end
 
  end
 
-  def cash_voucher()
+  def cash_voucher(payment, email)
     text ("\n")
     data =  [
-                 [{:content => '   '},'   ',{:content => 'Date:', :font_style => :bold, :align => :right},'   '],
-                 [{:content => 'Pay to', :font_style => :bold, :align => :left},{:content => '   '}],
-                 [{:content => 'Rupees', :font_style => :bold, :align => :left},{:content => '   '}],
-                 [{:content => 'By Cash/Cheque', :font_style => :bold, :align => :left},'  ',{:content => 'Dated', :font_style => :bold, :align => :left},'  '],
-                 [{:content => 'Being', :font_style => :bold, :align => :left},{:content => '   '}],
-                 [{:content => 'and Debit', :font_style => :bold, :align => :left},{:content => '   '}],
-                 [{:content => 'Prepared by', :font_style => :bold, :align => :left},{:content => 'Recd.Above Sum of Rs',:font_style => :bold, :align => :center},'   '],
+                 [{:content => 'Date: ' + payment.paid_on.strftime("%d/%m/%Y"), :font_style => :bold, :align => :right}],
+                 [{:content => 'Pay to: ' + payment.client.company_name, :font_style => :bold, :align => :left}],
+                 [{:content => 'Rupees: ' + payment.amount.to_s, :font_style => :bold, :align => :left}],
+                 [{:content => 'By: ' + payment.mode, :font_style => :bold, :align => :left}],
+                 [{:content => 'Being: ' + payment.description, :font_style => :bold, :align => :left}],
+                 [{:content => 'Prepared by: ' + email, :font_style => :bold, :align => :left}],
              ]
 
-    table(data, :column_widths => {0=>100,1=>250,2=>50,3=>50},
+    table(data, :column_widths => {0 => 450},
           :cell_style => {:border_width => 0, :height => 25, :inline_format => true, :padding => 3})
 
   end
 
- def receipt()
+ def receipt(payment, email)
    text ("\n")
    data =  [
-       [{:content => '   '},{:content => 'Date:', :font_style => :bold, :align => :right}],
-       [{:content => 'Received with thanks from', :font_style => :bold, :align => :left},{:content => '   '}],
-       [{:content => 'a sum of Rupees', :font_style => :bold, :align => :left},{:content => '   '}],
-       [{:content => 'towards Hiring/Others', :font_style => :bold, :align => :left},'   '],
-       [{:content => 'by Cash/Cheque/D.D', :font_style => :bold, :align => :left},{:content => '   '}],
-       [{:content => 'drawn on', :font_style => :bold, :align => :left},{:content => '   '}],
-       [{:content => 'Rs.____________', :font_style => :bold, :size => 12, :align => :left},{:content => 'Subject to realisation', :font_style => :bold, :align => :left}]
+       [{:content => 'Date: '+ payment.paid_on.strftime("%d/%m/%Y"), :font_style => :bold, :align => :right}],
+       [{:content => 'Received with thanks from: ' + payment.client.company_name, :font_style => :bold, :align => :left}],
+       [{:content => 'a sum of Rupees: ' + payment.amount.to_s(), :font_style => :bold, :align => :left}],
+       [{:content => 'towards: ' + payment.description, :font_style => :bold, :align => :left}],
+       [{:content => 'by: ' + payment.mode, :font_style => :bold, :align => :left}],
+       [{:content => 'Prepared by: ' + email, :font_style => :bold, :align => :left}]
    ]
 
-   table(data, :column_widths => {0=>150,1=>300},
+   table(data, :column_widths => {0 => 450},
          :cell_style => {:border_width => 0, :height => 25, :inline_format => true, :padding => 3})
 
  end
