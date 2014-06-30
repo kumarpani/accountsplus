a88e96ef354fae2a57023304a6f07f7a84e0b101
Pani <kumarpani@gmail.com>
2014-06-30 11:43:06 +0530

    [Pani]Backup data part 9

--- app/controllers/backup_controller.rb
+++ app/controllers/backup_controller.rb
@@ -18,6 +18,7 @@ class BackupController < ApplicationController
         p.use_shared_strings = true
 
         add_client_sheet(c, wb)
+        add_invoices_sheet(c.id, wb, params[:start_date], params[:end_date])
         add_payments_sheet(c.id, wb, params[:start_date], params[:end_date])
         add_ledger_sheet(c.id, wb, params[:start_date], params[:end_date])
         add_ist_sheet(c.id, wb, params[:start_date], params[:end_date])
@@ -92,23 +93,79 @@ class BackupController < ApplicationController
   end
 
   private def add_ist_sheet(client_id, wb, start_date, end_date)
-            wb.add_worksheet(name: "Incoming Service Taxes") do |sheet|
+    wb.add_worksheet(name: "Incoming Service Taxes") do |sheet|
 
-              sheet.add_row ['ID', 'Client ID', 'Invoice Number', 'Invoice Date', 'Description',
-                                  'Event Total', 'Service Tax', 'Created At', 'Updated At'],
-                            types: [:string, :string, :string, :string, :string, :string, :string, :string, :string]
+      sheet.add_row ['ID', 'Client ID', 'Invoice Number', 'Invoice Date', 'Description',
+                          'Event Total', 'Service Tax', 'Created At', 'Updated At'],
+                    types: [:string, :string, :string, :string, :string, :string, :string, :string, :string]
 
-              IncomingServiceTax.select {|i| i.client_id == client_id.to_s && i.created_at >= start_date && i.created_at <= end_date}
-                                .sort_by { |i| i[:invoice_date] }.each do |i|
+      IncomingServiceTax.select {|i| i.client_id == client_id.to_s && i.created_at >= start_date && i.created_at <= end_date}
+                        .sort_by { |i| i[:invoice_date] }.each do |i|
 
-                sheet.add_row [i.id, i.client_id, i.invoice_number, i.invoice_date.strftime('%d/%m/%Y'), i.description,
-                                  i.event_total, i.service_tax, i.created_at, i.updated_at],
-                              types: [:string, :string, :string, :string, :string, :string, :string, :string, :string]
+        sheet.add_row [i.id, i.client_id, i.invoice_number, i.invoice_date.strftime('%d/%m/%Y'), i.description,
+                          i.event_total, i.service_tax, i.created_at, i.updated_at],
+                      types: [:string, :string, :string, :string, :string, :string, :string, :string, :string]
 
-              end
+      end
 
-            end
-          end
+    end
+  end
 
+
+  private def add_invoices_sheet(client_id, wb, start_date, end_date)
+    wb.add_worksheet(name: "Invoices") do |sheet|
+
+      Quotation.select {|q| q.client_id == client_id && q.status == STATUS_INVOICE && q.created_at >= start_date && q.created_at <= end_date}
+            .sort_by { |q| q[:created_at] }.each do |q|
+
+        sheet.add_row ['ID', q.id, '', 'Client Id', q.client_id], types: [:string, :string, :string, :string, :string]
+        sheet.add_row ['Event Name', q.event_name, '', 'Venue', q.venue], types: [:string, :string, :string, :string, :string]
+        sheet.add_row ['Event Date', q.event_date.strftime('%d/%m/%Y'), '', 'Days', q.days], types: [:string, :string, :string, :string, :string]
+        sheet.add_row ['Invoice Type', q.invoice_type, '', 'Status', q.status], types: [:string, :string, :string, :string, :string]
+        sheet.add_row ['Invoice Number', q.invoice_number, '', 'Invoice Raised Date', q.invoice_raised_date.strftime('%d/%m/%Y')], types: [:string, :string, :string, :string, :string]
+        sheet.add_row ['Order Placed By', q.order_placed_by, '', 'Invoice Raised By', q.invoice_raised_by], types: [:string, :string, :string, :string, :string]
+        sheet.add_row ['Created At', q.created_at, '', 'Updated At', q.updated_at], types: [:string, :string, :string, :string, :string]
+        sheet.add_row ['notes', q.notes], types: [:string, :string]
+        sheet.add_row ['Terms and Conditions', q.tac], types: [:string, :string]
+
+        sheet.add_row [''], types: [:string]
+
+        add_item_details(q, sheet)
+
+        sheet.add_row [''], types: [:string]
+        sheet.add_row [''], types: [:string]
+        sheet.add_row [''], types: [:string]
+
+
+      end
+
+    end
+  end
+
+
+  private def add_item_details(q, sheet)
+    sheet.add_row ['ID', 'Particulars', 'Quantity', 'Days', 'Unit Price', 'Price', 'Item Group Name']
+
+    q.item_details.sort_by {|i| i[:item_group_name]}.each do |i|
+      sheet.add_row [i.id, i.particulars, i.quantity, i.days,
+                     i.unit_price, i.price, i.item_group_name],
+                    types: [:string, :string, :string, :string, :string, :string, :string]
+    end
+
+    sheet.add_row ['', 'Total:', '', '', '', ApplicationController.helpers.number_with_precision(q.total_item_price, :precision => 2)],
+        types: [:string, :string, :string, :string, :string, :string]
+
+    if q.is_a_complete_tax_invoice? || q.is_a_complete_tax_exempted_invoice?
+      sheet.add_row ['', 'Service Tax @ 12%', '', '', '', q.service_tax_at_12_percent],
+                    types: [:string, :string, :string, :string, :string, :string]
+      sheet.add_row ['', 'Education Cess @ 2% (On S.T)', '', '', '', q.education_cess],
+                    types: [:string, :string, :string, :string, :string, :string]
+      sheet.add_row ['', 'Sec. & Higher Edu. Cess @ 1% (On S.T)', '', '', '', q.higher_education_cess],
+                    types: [:string, :string, :string, :string, :string, :string]
+      sheet.add_row ['', 'Total with taxes (rounded off):', '', '', '', ApplicationController.helpers.number_with_precision(q.total_price, :precision => 2)],
+                    types: [:string, :string, :string, :string, :string, :string]
+
+    end
+  end
 
 end
