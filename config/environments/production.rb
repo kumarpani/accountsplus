472d78eb50563660de8154a44747f4df4af097ad
kavya <kavyanagaraj.p@gmail.com>
2014-09-15 17:55:25 +0530

    Initial commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Accountsplus::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'audioplus.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'testvagrant.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
