98802d6efcbda32a1ec2d7c66a00761ba94c9771
Pani Kumar <kumarpani@gmail.com>
2015-06-08 23:18:43 +0530

    [Pani] Renamed image file

--- app/pdfs/print_quotation.rb
+++ app/pdfs/print_quotation.rb
@@ -255,7 +255,7 @@ class PrintQuotation < PrintBase
 
   def signature
     text("\nFor #{ApplicationHelper::NAME}\n\n\n\n")
-    image "#{Rails.root}/app/assets/images/signature.jpg", height: 50
+    image "#{Rails.root}/app/assets/images/sealandsign.jpg", height: 50
     text("Authorized Signatory")
 
   end
