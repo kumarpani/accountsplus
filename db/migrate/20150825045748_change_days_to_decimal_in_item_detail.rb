d3df42580acee350080235d9bee541ec6dfae83d
Pani Kumar <kumarpani@gmail.com>
2015-08-27 10:04:34 +0530

    [Pani] Updated the precision in days

--- db/migrate/20150825045748_change_days_to_decimal_in_item_detail.rb
+++ db/migrate/20150825045748_change_days_to_decimal_in_item_detail.rb
@@ -1,5 +1,5 @@
 class ChangeDaysToDecimalInItemDetail < ActiveRecord::Migration
   def change
-    change_column :item_details, :days, :decimal, :precision => 3, :scale => 2
+    change_column :item_details, :days, :decimal, :precision => 5, :scale => 2
  end
 end
